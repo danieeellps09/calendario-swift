c2c13d962a9b0adef6b578b08bffda6501a03115
Daniel Lopes <daniel-lps@hotmail.com.br>
2024-08-06 19:00:12 -0300

    Initial Commit

---
+++ Calendario/CalendarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarioApp.swift
+//  Calendario
+//
+//  Created by Daniel Lopes da Silva on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CalendarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
